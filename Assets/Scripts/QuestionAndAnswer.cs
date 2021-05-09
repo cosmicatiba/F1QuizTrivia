ed23f827c9216b601146896d3b6877764dff91c2
Erdinç <Erdin_uyar@hotmail.com>
2021-05-10 01:13:07 +0300

    Scripts added

---
+++ Assets/Scripts/QuestionAndAnswer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class questionandanswers
+{
+    public string question;
+    public string[] answers;
+    public int correctanswers;
+
+}
---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<questionandanswers> QnA;
+    public GameObject[] options;
+    public int currentquestion;
+    public Text questionTxt;
+
+    private void Start()
+    {
+        generatequestion();
+    
+    }
+    void Setanswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].transform.GetChild(0).GetComponent <Text>().text= QnA[currentquestion].answers[i];
+        }
+    }
+    void generatequestion()
+    {
+        currentquestion = Random.Range(0, QnA.Count);
+        questionTxt.text = QnA[currentquestion].question;
+    }
+
+}
+
+
