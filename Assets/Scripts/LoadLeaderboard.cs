571d8de1c03532d8eae4216ba50431d8592bb7f5
Deniz Soysal <sezerdeniz.soysal@gmail.com>
2021-05-10 00:24:48 +0300

    MainMenu

---
+++ Assets/Scripts/LoadLeaderboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLeaderboard : MonoBehaviour
+{
+    public void OpenLevel()
+    {
+        SceneManager.LoadScene("Leaderboard");
+    }
+}
